661c61ae0cdc70469a4bc2ea717fdec159653744
Sean-Potts <Sean-potts@outlook.com>
2023-11-07 13:56:31 -0500

    created file structure for controllers and routes so post requests are clean inside the routes/posts.js file.

--- server/index.js
+++ server/index.js
@@ -2,18 +2,25 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRoutes);
+
+
 
 app.use(bodyParser.json({limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://seanpotts:<@vN#ALE9k_Jhtw9>@cluster0.zyk9qmj.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://seanpotts:CentrumSophia123@cluster0.zyk9qmj.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {usenNewUrlParser: true, useUnifiedTopology: true})
+
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
     .catch((error)=> console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+
