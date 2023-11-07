ed6fc7fbee722f538dfc025ba5be31fccdf5a722
Sean-Potts <Sean-potts@outlook.com>
2023-11-07 13:28:16 -0500

    configuring index.js file for mongooseDB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://seanpotts:<@vN#ALE9k_Jhtw9>@cluster0.zyk9qmj.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {usenNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+    .catch((error)=> console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
