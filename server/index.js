21e7bfd3a0e9e4ba501a27e51c7074f087065b61
Sean-Potts <Sean-potts@outlook.com>
2023-11-08 12:34:59 -0500

    started to do post.js for loading posts from the database

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
-import bodyParser from "body-parser";
 import express from "express";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
 import postRoutes from "./routes/posts.js";
 
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
+
 app.use("/posts", postRoutes);
+
 const CONNECTION_URL =
   "mongodb+srv://seanpotts:CentrumSophia123@cluster0.zyk9qmj.mongodb.net/?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(CONNECTION_URL, {})
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((error) => console.log(error.message));
+  .catch((error) => console.log(`${error} did not connect`));
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { getPosts, createPosts } from '../controllers/posts.js';
+import express from "express";
+import { getPosts, createPosts } from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get('/', getPosts);
-router.get('/', createPosts);
+router.get("/", getPosts);
+router.post("/", createPosts);
 export default router;
