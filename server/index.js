2b342c916c83b3d035c2a89a2483150ef94092de
Sean-Potts <Sean-potts@outlook.com>
2023-12-12 14:15:10 -0500

    adding connection env

--- server/index.js
+++ server/index.js
@@ -11,7 +11,17 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["highlight-mern.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json("APP is RUNNING.");
+});
 
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
