03a9ae7e76f0b77051cf1e6e87460a92a862e70a
Sean-Potts <Sean-potts@outlook.com>
2023-12-12 16:11:16 -0500

    adding connection env

--- server/index.js
+++ server/index.js
@@ -22,8 +22,6 @@ app.use(
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(express.json());
-
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
