a274b6d6570461b80af5962732bae487ecbae378
Sean-Potts <Sean-potts@outlook.com>
2023-12-12 15:31:06 -0500

    adding connection env

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://highlight-mern.vercel.app/" });
+const API = axios.create({ baseURL: "https://highlight-mern.vercel.app" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
