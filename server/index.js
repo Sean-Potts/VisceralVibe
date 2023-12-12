8049d1de463178e72f152e2cc925ad81bc453a1f
Sean-Potts <Sean-potts@outlook.com>
2023-12-12 15:17:52 -0500

    adding connection env

--- server/index.js
+++ server/index.js
@@ -5,19 +5,23 @@ import cors from "cors";
 import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
+
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(
-  cors({
-    origin: ["https://highlight-mern-frontend.vercel.app/posts/"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://highlight-mern-frontend.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 
 app.use("/posts", postRoutes);
